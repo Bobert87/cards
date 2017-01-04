fe860f4d84a3cc294d1ddbeaec5d291a2a1f38ee
Cristian Prieto <me@cprieto.com>
2017-01-04 11:12:23 +1100

    Syntax changes
    It _always_ needs a ; after the 'use strict' sentence.
    
    When directly declaring an export, a named function is not required because the name will be overwritten anyway, so instead of:
    
    ```js
    module.exports.myFunc = function myFunc(blah) {}
    ```
    
    is enough and much more readable:
    
    ```js
    module.exports.myFunc = function (name) {}
    ```

--- src/core/utils/utils.js
+++ src/core/utils/utils.js
@@ -1,16 +1,19 @@
-'use strict'
+'use strict';
+
 module.exports.ENV = {
     port:3001
 };
 
-module.exports.getSetup = function getSetup(name) {
+module.exports.getSetup = function (name) {
     let root = '../config';
     let path = '/DC';
+    
     switch (name.toUpperCase()) {
         case 'DC':
             path = '/DC';
             break;
     }
+    
     let setup = require(root + path + '/setup.js').Setup;
     let Powers = require(root + path + '/powers.js');
     setup.powers = new Powers();
